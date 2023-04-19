f71ccf4b54c92fd0eefac1b51d8cd8355ddf58b6
zxcv5595 <zxcv5595@korea.ac.kr>
2023-04-19 23:02:48 +0900

    Controller->Service ->Repository 구조의 아키텍쳐, add creatAccount(), getAccount()

---
+++ src/main/java/com/example/account/service/AccountService.java
@@ -0,0 +1,29 @@
+package com.example.account.service;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountStatus;
+import com.example.account.repository.AccountRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService { // Service -> Repository
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    public void createAccount(){
+        Account account = Account.builder()
+                .accountNumber("40000")
+                .accountStatus(AccountStatus.IN_USE)
+                .build();
+        accountRepository.save(account);
+    }
+
+    @Transactional
+    public Account getAccount(Long id){
+        return accountRepository.findById(id).get();
+    }
+
+}
