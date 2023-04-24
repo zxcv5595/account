2ce0104c1dd15587c9361923cb800e61880aa1a8
zxcv5595 <zxcv5595@korea.ac.kr>
2023-04-24 20:10:24 +0900

    Complete GetQueryTransaction API in Transaction, test also

--- src/test/java/com/example/account/controller/TransactionControllerTest.java
+++ src/test/java/com/example/account/controller/TransactionControllerTest.java
@@ -1,8 +1,10 @@
 package com.example.account.controller;
 
+import com.example.account.dto.AccountDto;
 import com.example.account.dto.TransactionDto;
 import com.example.account.dto.UseBalance;
 import com.example.account.service.TransactionService;
+import com.example.account.type.TransactionType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.example.account.type.TransactionResultType.S;
+import static com.example.account.type.TransactionType.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -60,5 +66,42 @@ class TransactionControllerTest {
                 .andExpect(jsonPath("$.amount").value(12345));
     }
 
+    @Test
+    void SuccessGetQueryTransaction() throws Exception {
+        //given
+        List<AccountDto> accountDtos = Arrays.asList(
+                AccountDto.builder()
+                        .accountNumber("1234567890")
+                        .balance(1000L)
+                        .build(),
+                AccountDto.builder()
+                        .accountNumber("1111111111")
+                        .balance(1001L)
+                        .build(),
+                AccountDto.builder()
+                        .accountNumber("2222222222")
+                        .balance(1002L)
+                        .build()
+        );
+        given(transactionService.queryTransaction(anyString()))
+                .willReturn(TransactionDto.builder()
+                        .accountNumber("1000000000")
+                        .transactionType(USE)
+                        .transactedAt(LocalDateTime.now())
+                        .amount(12345L)
+                        .transactionId("transactionId")
+                        .transactionResultType(S)
+                        .build());
+        //when
+        //then
+        mockMvc.perform(get("/transaction/12345"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountNumber").value("1000000000"))
+                .andExpect(jsonPath("$.transactionResult").value("S"))
+                .andExpect(jsonPath("$.transactionType").value("USE"))
+                .andExpect(jsonPath("$.transactionId").value("transactionId"))
+                .andExpect(jsonPath("$.amount").value(12345));
+    }
 
 }
--- src/test/java/com/example/account/service/TransactionServiceTest.java
+++ src/test/java/com/example/account/service/TransactionServiceTest.java
@@ -416,4 +416,47 @@ class TransactionServiceTest {
         assertEquals(ErrorCode.TOO_OLD_ORDER_TO_CANCEL, exception.getErrorCode());
     }
 
+    @Test
+    void successQueryTransaction(){
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .name("Pobi").build();
+        Account account = Account.builder()
+                .id(1L)
+                .accountUser(user)
+                .accountStatus(IN_USE)
+                .balance(10000L)
+                .accountNumber("1000000012").build();
+        Transaction transaction = Transaction.builder()
+                .account(account)
+                .transactionType(USE)
+                .transactionResultType(S)
+                .transactionId("transactionId")
+                .transactedAt(LocalDateTime.now().minusYears(1).minusDays(1))
+                .amount(CANCEL_AMOUNT)
+                .balanceSnapshot(9000L)
+                .build();
+        given(transactionRepository.findByTransactionId(anyString()))
+                .willReturn(Optional.of(transaction));
+        //when
+        TransactionDto transactionDto = transactionService.queryTransaction("trxId");
+        //then
+        assertEquals(USE,transactionDto.getTransactionType());
+        assertEquals(S,transactionDto.getTransactionResultType());
+        assertEquals("transactionId",transactionDto.getTransactionId());
+    }
+
+    @Test
+    @DisplayName("원 거래 없음 - 거래 조회 실패")
+    void queryTransaction_NotFoundTransaction() {
+        //given
+        given(transactionRepository.findByTransactionId(anyString()))
+                .willReturn(Optional.empty());
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.queryTransaction("transactionId"));
+        //then
+        assertEquals(ErrorCode.TRANSACTION_NOT_FOUND, exception.getErrorCode());
+    }
 }
